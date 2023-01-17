9ec588a8b7204a788ef5483fccf06bc32c227d79
NadirovVladimir <nadirovvova56@gmail.com>
2023-01-17 16:51:12 +0400

    added modal

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,12 +1,15 @@
-import React, { Component } from "react";
-import classNames from "classnames/bind";
+import { Component } from "react";
 import styles from "./Aside.module.scss"
+import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles)
 
-export class Aside extends Component{
-    render(){
-        const {isOpen} = this.props
-        return <aside className={cx('aside',{aside_open:isOpen})} />
+class Aside extends Component {
+    render() {
+        const {isOpenAside} = this.props
+        return (
+            <aside className={cx("aside", { open: isOpenAside})} />
+        )
     }
 }
+export default Aside;
